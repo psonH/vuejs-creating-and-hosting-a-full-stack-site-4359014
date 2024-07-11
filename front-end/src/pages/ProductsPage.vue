ebd57fc3ba0f40bf5ef793cdb2a5949be9375044
Priyank Hajela <35231175+psonH@users.noreply.github.com>
2024-07-11 16:40:03 +0000

    front-end code

---
+++ front-end/src/pages/ProductsPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Products</h1>
+  <div class="grid-wrap">
+    <div v-for="product in products" v-bind:key="product.id in products">
+      <img :src="product.imageName" />
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { products } from '../temp-data';
+  export default {
+    name: 'ProductsPage',
+    components: {},
+    data() {
+      return {
+        products,
+      }
+    }
+  }
+</script>
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Shopping Cart Page</h1>
+
+</template>
+
+<script>
+  export default {
+    name: 'ShoppingCartPage',
+    components: {}
+  }
+</script>
